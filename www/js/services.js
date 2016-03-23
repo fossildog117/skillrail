a1be1f6f6b8a17a3194ab508f08ad27086028ef4
fossildog117 <quantumbiology@hotmail.co.uk>
2016-03-23 19:33:58 +0000

    Update services.js

--- www/js/services.js
+++ www/js/services.js
@@ -1,11 +1,24 @@
 angular.module('app.services', ['ngResource'])
 
-  .factory('Post', function ($resource) {
-    return $resource('http://skillraildemo.azurewebsites.net/Token');
-  })
+    .factory('Post', function ($resource) {
+        return $resource('http://skillraildemo.azurewebsites.net/Token');
+    })
 
 
+    .factory('SuggestedJobsServ', function ($resource) {
+        return $resource('http://skillraildemo.azurewebsites.net/api/Products');
+    })
 
-  .factory('SuggestedJobsServ', function($resource) {
-  return $resource('http://skillraildemo.azurewebsites.net/api/Products');
-})
+    .service('Token', function () {
+
+        var Token = 'Token';
+
+        return {
+            getProperty: function () {
+                return Token;
+            },
+            setProperty: function (value) {
+                Token = value;
+            }
+        };
+    });
